da1f0395ce888794fec83d3f41e810b493a73c4f
Alexandre <alex.3385@hotmail.fr>
2016-02-01 10:30:33 +0100

    Create libHotels

--- Frontend/libHotels/Models/HotelContext.cs
+++ Frontend/libHotels/Models/HotelContext.cs
@@ -1,28 +1,27 @@
-﻿namespace libAirports.Models
+﻿namespace libHotels.Models
 {
-    using System.ComponentModel.DataAnnotations;
     using System.Data.Entity;
 
-    public class AirportContext : DbContext
+    public class HotelContext : DbContext
     {
-        // Votre contexte a été configuré pour utiliser une chaîne de connexion « AirportContext » du fichier 
+        // Votre contexte a été configuré pour utiliser une chaîne de connexion « HotelContext » du fichier 
         // de configuration de votre application (App.config ou Web.config). Par défaut, cette chaîne de connexion cible 
-        // la base de données « libAirports.Models.AirportContext » sur votre instance LocalDb. 
+        // la base de données « libHotels.Models.HotelContext » sur votre instance LocalDb. 
         // 
         // Pour cibler une autre base de données et/ou un autre fournisseur de base de données, modifiez 
-        // la chaîne de connexion « AirportContext » dans le fichier de configuration de l'application.
-        public AirportContext()
-            : base("name=AirportContext")
+        // la chaîne de connexion « HotelContext » dans le fichier de configuration de l'application.
+        public HotelContext()
+            : base("name=HotelContext")
         {
         }
 
         // Ajoutez un DbSet pour chaque type d'entité à inclure dans votre modèle. Pour plus d'informations 
         // sur la configuration et l'utilisation du modèle Code First, consultez http://go.microsoft.com/fwlink/?LinkId=390109.
 
-         public virtual DbSet<Airport> MyAirports { get; set; }
+        public virtual DbSet<Hotel> MyHotels { get; set; }
     }
 
-    public class Airport
+    public class Hotel
     {
         public int Id { get; set; }
         public string Name { get; set; }
---
+++ Frontend/libHotels/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("libHotels")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("libHotels")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("6b8b62e0-5a95-4918-8588-780580240ac5")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
