2cc744d9a17afcddb982abf9f8d6e41b54269f2a
Alexandre <alex.3385@hotmail.fr>
2016-02-01 13:38:03 +0100

    Add pusher to services

---
+++ Frontend/pusher/ReservationService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pusher
+{
+    using pusher.data;
+
+    public class ReservationService
+    {
+
+        private const String QUEUE_IP = "127.0.0.1";
+        private const String QUEUE_PATH = "FormatName:Direct=TCP:" + QUEUE_IP + @"\private$\dotNetBooking";
+
+        public static void book(HotelReservation hotel, FlightReservation flight)
+        {
+            Reservation reservation = new Reservation();
+            reservation.Flight = flight;
+            reservation.Hotel = hotel;
+
+            MessageQueue queue = new MessageQueue(QUEUE_PATH);
+            queue.Send(reservation);
+        }
+
+    }
+}
---
+++ Frontend/pusher/data/Reservation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pusher.data
+{
+    public class Reservation
+    {
+        public HotelReservation Hotel { get; set; }
+        public FlightReservation Flight { get; set; }
+    }
+
+    public class HotelReservation
+    {
+        public int HotelId { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public DateTime DepartureDate { get; set; }
+    }
+
+    public class FlightReservation
+    {
+        public int FlightId { get; set; }
+        public int HotelId { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public DateTime DepartureDate { get; set; }
+    }
+}
