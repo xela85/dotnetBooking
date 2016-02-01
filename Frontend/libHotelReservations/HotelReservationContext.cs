bdd07ea5920f849140b7fc7dc961cd16d16d861e
Alexandre <alex.3385@hotmail.fr>
2016-02-01 10:51:27 +0100

    Add libHotelReservations

---
+++ Frontend/libHotelReservations/HotelReservationContext.cs
@@ -0,0 +1,32 @@
+﻿namespace libHotelReservations
+{
+    using System;
+    using libHotels.Models;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class HotelReservationContext : DbContext
+    {
+        // Votre contexte a été configuré pour utiliser une chaîne de connexion « HotelReservationContext » du fichier 
+        // de configuration de votre application (App.config ou Web.config). Par défaut, cette chaîne de connexion cible 
+        // la base de données « libHotelReservations.HotelReservationContext » sur votre instance LocalDb. 
+        // 
+        // Pour cibler une autre base de données et/ou un autre fournisseur de base de données, modifiez 
+        // la chaîne de connexion « HotelReservationContext » dans le fichier de configuration de l'application.
+        public HotelReservationContext()
+            : base("name=HotelReservationContext")
+        {
+        }
+
+        // Ajoutez un DbSet pour chaque type d'entité à inclure dans votre modèle. Pour plus d'informations 
+        // sur la configuration et l'utilisation du modèle Code First, consultez http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    public class HotelReservation
+    {
+        public int Id { get; set; }
+        public Hotel Hotel { get; set; }
+    }
+}
