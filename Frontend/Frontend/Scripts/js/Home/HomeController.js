d7a2515929c0ce0ddcd74b80716947a627569cf3
naokha <xavier.huberdeau@gmail.com>
2016-02-01 15:22:58 +0100

    Marker example

--- Frontend/Frontend/Scripts/js/Home/HomeController.js
+++ Frontend/Frontend/Scripts/js/Home/HomeController.js
@@ -1,4 +1,24 @@
 ﻿app.controller('HomeController', function ($scope) {
-    $scope.map = { center: { latitude: 47.282949, longitude: -1.521396 }, zoom: 8 };
-    
+    $scope.markers = [
+        {
+            id: 1,
+            coords: {
+                latitude: 51.4775, longitude: -0.461389
+            }
+        },
+        {
+            id: 2,
+            coords: {
+                latitude: 68.534444, longitude: -89.808056
+            }
+        },
+        {
+            id: 3,
+            coords: {
+                latitude: 28.200881, longitude: 83.982056
+            }
+        }
+    ];
+    $scope.map = { center: { latitude: 47.282949, longitude: -1.521396}, zoom: 8 };
+
 });
