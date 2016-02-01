29d89deae8036e05d248991f543b8f960c78f068
Alexandre <alex.3385@hotmail.fr>
2016-02-01 16:41:13 +0100

    Use transactionScope to handle errors Create flight reservations DAO

--- Frontend/applicationServer/puller.cs
+++ Frontend/applicationServer/puller.cs
@@ -4,6 +4,8 @@ using System.ServiceProcess;
 using data.messaging;
 using libHotelReservations.Models;
 using System.Messaging;
+using libFlightReservations.Models;
+using System.Transactions;
 
 namespace applicationServer
 {
@@ -11,6 +13,7 @@ namespace applicationServer
     {
         private const float SLEEP_MINUTES = 0.5F;
         private IHotelReservationsApi hotels = new HotelReservationDb();
+        private IFlightReservationsApi flights = new FlightReservationDb();
         private EventLog logger;
 
         public Puller()
@@ -38,26 +41,41 @@ namespace applicationServer
         public void readAndPeek(object sender, ReceiveCompletedEventArgs args)
         {
             MessageQueue queue = (MessageQueue)sender;
-            try { 
-            this.logger.WriteEntry("started peek on " + Reservation.QUEUE_PATH);
-            
-            Message msg = queue.EndReceive(args.AsyncResult);
-            msg.Formatter = new XmlMessageFormatter(new Type[] { typeof(Reservation) });
-            Reservation reservation = (Reservation) msg.Body;
+            try
+            {
+                this.logger.WriteEntry("started peek on " + Reservation.QUEUE_PATH);
 
-            this.logger.WriteEntry("found hotel " + reservation.Hotel.HotelId);
-
-            var hotelRes = new libHotelReservations.Models.HotelReservation();
-            hotelRes.DepartureDate = reservation.Hotel.DepartureDate;
-            hotelRes.ArrivalDate = reservation.Hotel.ArrivalDate;
-            hotelRes.HotelId = reservation.Hotel.HotelId;
-            hotels.book(hotelRes);
-            this.logger.WriteEntry("peek done");
-            } catch(Exception e)
+                Message msg = queue.EndReceive(args.AsyncResult);
+                msg.Formatter = new XmlMessageFormatter(new Type[] { typeof(Reservation) });
+                Reservation reservation = (Reservation)msg.Body;
+                this.logger.WriteEntry("found hotel " + reservation.Hotel.HotelId);
+                saveReservation(reservation);
+                this.logger.WriteEntry("peek done");
+            }
+            catch (Exception e)
             {
                 this.logger.WriteEntry(e.Message);
             }
             queue.BeginReceive();
         }
+
+        private void saveReservation(Reservation reservation)
+        {
+            using (var txScope = new TransactionScope())
+            {
+                var hotelRes = new libHotelReservations.Models.HotelReservation();
+                hotelRes.DepartureDate = reservation.Hotel.DepartureDate;
+                hotelRes.ArrivalDate = reservation.Hotel.ArrivalDate;
+                hotelRes.HotelId = reservation.Hotel.HotelId;
+                hotels.book(hotelRes);
+
+                var flightRes = new libFlightReservations.Models.FlightReservation();
+                flightRes.DepartureDate = reservation.Flight.DepartureDate;
+                flightRes.ArrivalDate = reservation.Flight.ArrivalDate;
+                flightRes.FlightId = reservation.Flight.FlightId;
+                flights.book(flightRes);
+            }
+        }
+
     }
 }
