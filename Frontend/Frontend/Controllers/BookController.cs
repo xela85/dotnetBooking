a60e3c0e1c5cbaf28396bb4be93fce221df18b3e
CHIHEB Faouzi <chiheb.faouzi@live.fr>
2016-02-03 12:01:46 +0100

    no message

---
+++ Frontend/Frontend/Controllers/BookController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using pusher;
+using data.messaging;
+namespace frontend.Controllers
+{
+    public class BookController : ApiController
+    {
+        [HttpPost]
+        [Route("api/book/")]
+        public String Book(HotelReservation hotelReservation, FlightReservation flightReservation, User )
+        {
+            return "La réservation ;
+        }
+        [HttpGet]
+        [Route("api/airports/")]
+        public IEnumerable<Airport> All()
+        {
+            return airports.GetAirports();
+        }
+    }
+    public class Error
+    {
+        public String Message {get;set;}
+        public Exception Exception {get;set;}
+    }
+}
